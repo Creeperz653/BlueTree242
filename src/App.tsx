be3a5039f83bcaa7fbd3b174f7cef520d090f1bb
BlueTree242 <yahiamsm2@gmail.com>
2022-04-06 00:43:22 +0200

    remove unused stuff in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import Header from "./components/header/header"
-import About from './components/about/about'
-import {Route, Switch, useLocation} from 'wouter';
+import {Route, Switch} from 'wouter';
 import {useState} from "react";
 import Index from "./pages";
 
 export default function App() {
-    const [location, setLocation] = useState<string | undefined>(undefined);
+    const [location] = useState<string | undefined>(undefined);
 
     return (
         <>
