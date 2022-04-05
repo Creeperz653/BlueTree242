922b2801c22c04a500d6e801b0677e30980d2ea5
BlueTree242 <yahiamsm2@gmail.com>
2022-04-06 00:49:05 +0200

    fix small spelling mistake

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -5,7 +5,7 @@ export default function About() {
             <h1>Hey! I am BlueTree242!!</h1>
             <p>I am a Developer interested in Java, Typescript and React! I Am Always interested on learning new modern stuff and usually attempt to learn them, for example React (this is my first website in react 😀).
                 <br />
-                I focus mostly on Discord & Minecraft Projects, but i sometimes to other stuff!
+                I focus mostly on Discord & Minecraft Projects, but i sometimes i do other stuff!
             <br /> <br />
                 <strong>You can find my github <a href={"https://github.com/BlueTree242"} target={"_blank"}>here</a></strong>
             </p>
